de90e20f15376eb680d650c9371293ca42d342b2
royong <fhdyd6039@gmail.com>
2018-12-27 13:19:19 +0900

    [곽로용]곱셈계산

---
+++ src/test/git/Test2_Ry.java
@@ -0,0 +1,15 @@
+package test.git;
+
+public class Test2_Ry {
+
+	public static void main(String[] args) {
+		for(int i=2;i<10;i++) {
+			for(int j=1;j<10;j++) {
+				int result = i*j;
+				System.out.println(i +"*"+ j + "=" + result);
+			}
+			System.out.println("-------------------");
+		}
+	}
+
+}
