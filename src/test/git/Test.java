87c8f058a5e2f59a6bb3fda247b7a31982a97de2
naim <pinktor157@gmail.com>
2018-12-27 12:59:10 +0900

    Test.java 하나 만듦

---
+++ src/test/git/Test.java
@@ -0,0 +1,15 @@
+package test.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		int result = a + b;
+		
+		System.out.println(result);
+	}
+
+}
